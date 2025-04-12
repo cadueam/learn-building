61d81560d8d713a0e514e249b307a5bd29dc96bf
Cadu Andrade <cadu.eam@gmail.com>
2025-04-12 09:33:08 -0300

    Almost done with js, soon will be back to styling and add sqlite :)

--- 01.TodoList/js/main.js
+++ 01.TodoList/js/main.js
@@ -1,12 +1,10 @@
 const btnsubmitTask = document.getElementById("btn-submittask"); // botão enviar a task
 const inputTask = document.getElementById("input-task"); // campo de seleção da tarefa
 const taskField = document.getElementById("all-tasks");
-const btnDelete = document.getElementById("btn-delete");
+const allDelButtons = document.getElementsByClassName("btn-delete");
 const task = document.querySelectorAll("taskfield");
 
 const taskArr = [];
-
-//TODO: TAREFAS ESTÃO DUPLICANDO, 1 + 1 + 2
 
 class Tarefas {
   constructor(task, data, id) {
@@ -20,7 +18,7 @@ class Tarefas {
     createDiv.Id = "task";
 
     createDiv.innerHTML = `
-          <p id="task-id">${this.id}<span><button type="submit" class="btn-delete" id="btn-delete">
+          <p id="task-id">${this.id}<span><button type="submit" class="btn-delete" >
                 Delete
               </button></span>
           </p>
@@ -30,12 +28,23 @@ class Tarefas {
     return createDiv;
   }
 }
-
 //TODO: ADD SCRIPT TO DEL BTN , LOOP ARR SEE WICH DEL WAS PRESSED AND DELETE ITS DIV
-
+//STILL NOT FIGURED IT OUT ON HOW TO LOOP THAT BTN DONT WANT TO USE GPT SO IM STOPPING FOR A MINUTE AND GOING TO CLEAN THE HOUSE UHUU
 btnsubmitTask.addEventListener("click", () => {
   let taskValue = inputTask.value;
-  let newTask = new Tarefas(taskValue, "0", taskArr.length + 1);
-  let sendTask = taskArr.push(newTask);
-  newTask.returnTask();
+
+  if (!taskValue) {
+    return alert("We need an input");
+  } else {
+    let newTask = new Tarefas(taskValue, "0", taskArr.length + 1);
+    let sendTask = taskArr.push(newTask);
+    newTask.returnTask();
+    for (let i = 0; i < allDelButtons.length; i++) {
+      allDelButtons[i].addEventListener("click", () => {
+        console.log(`hello im btn ${i + 1}`);
+      });
+    }
+  }
 });
+
+console.log(allDelButtons);
