262a8d356a87a4492ce19b505749aee600e39151
Cadu Andrade <cadu.eam@gmail.com>
2025-04-14 00:36:50 -0300

    I HAve no idea what i did.

--- 01.TodoList/js/main.js
+++ 01.TodoList/js/main.js
@@ -3,7 +3,6 @@ const inputTask = document.getElementById("input-task"); // campo de seleção d
 const taskField = document.getElementById("all-tasks");
 const allDelButtons = document.getElementsByClassName("btn-delete");
 const task = document.querySelectorAll("taskfield");
-
 const taskArr = [];
 
 class Tarefas {
@@ -25,11 +24,30 @@ class Tarefas {
           <p id="task-title">${this.tarefa}</p>
         </div>`;
     taskField.appendChild(createDiv);
+
+    const deleteBtn = createDiv.querySelector(".btn-delete");
+    deleteBtn.addEventListener("click", () => {
+      createDiv.remove();
+
+      const taskIndex = taskArr.findIndex((item) => item.id === this.id);
+      if (taskIndex !== -1) {
+        taskArr.splice(taskIndex, 1);
+      }
+
+      console.log("Tarefa deletada. Estado atual do array:", taskArr);
+    });
     return createDiv;
   }
 }
+
 //TODO: ADD SCRIPT TO DEL BTN , LOOP ARR SEE WICH DEL WAS PRESSED AND DELETE ITS DIV
 //STILL NOT FIGURED IT OUT ON HOW TO LOOP THAT BTN DONT WANT TO USE GPT SO IM STOPPING FOR A MINUTE AND GOING TO CLEAN THE HOUSE UHUU
+function addTasks(taskArr) {
+  taskArr.forEach((task) => {
+    task.returnTask();
+  });
+}
+
 btnsubmitTask.addEventListener("click", () => {
   let taskValue = inputTask.value;
 
@@ -37,14 +55,11 @@ btnsubmitTask.addEventListener("click", () => {
     return alert("We need an input");
   } else {
     let newTask = new Tarefas(taskValue, "0", taskArr.length + 1);
-    let sendTask = taskArr.push(newTask);
+    taskArr.push(newTask);
     newTask.returnTask();
-    for (let i = 0; i < allDelButtons.length; i++) {
-      allDelButtons[i].addEventListener("click", () => {
-        console.log(`hello im btn ${i + 1}`);
-      });
-    }
+    inputTask.value = "";
   }
 });
 
+console.log(taskArr);
 console.log(allDelButtons);
